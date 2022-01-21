9fa50f9d61ab99b82b85d608e514a5d87c4eb71c
Keith-Swamy <97607209+Keith-Swamy@users.noreply.github.com>
2022-01-21 19:55:32 +0530

    Add files via upload

---
+++ asg2_21105097 Output file.py
@@ -0,0 +1,51 @@
+Solution to Question-1 
+
+The length of the string is :  35
+The reversed string is :  egaugnal evitisnes esac a si nohtyP
+The new string is :  a case sensitive
+The replaced string will be :  Python is object oriented language
+The index of 'a' in the given String is :  10
+The given input without white spaces is :  Pythonisacasesensitivelanguage
+
+
+Solution to Question-2 
+
+Enter your name : Keith Swamy
+Enter SID : 21105097
+Enter department name : ECE
+Enter CGPA : 9.9
+
+Hey, Keith Swamy Here!
+My SID is 21105097
+I am from ECE department and my CGPA is 9.9
+
+
+Solution to Question-3 
+
+Value of a & b is : 8
+Value of a | b is : 58
+Value of a ^ b is : 50
+Left shift both a and b with 2 bits : a= 224, b=40
+Right shift a with 2 bits and b with 4 bits : a= 14, b=0
+
+
+Solution to Question-4 
+
+Enter first number : 45
+Enter second number : 98
+Enter third number : 6
+The greatest of the three numbers is : 98
+
+
+Solution to Question-5 
+
+Enter String : His name is Rahul
+Yes
+
+
+Solution to Question-6 
+
+Enter length of first side of the triangle : 35.5
+Enter length of second side of the triangle : 40
+Enter length of third side of the triangle : 19
+Yes
